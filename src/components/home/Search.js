0598fcab68b953f04190ea7797541184852067b2
thaolethithu1999 <thao.lethithu2502@gmail.com>
2022-05-07 18:48:44 +0700

    show empty notification in searching

--- src/components/home/Search.js
+++ src/components/home/Search.js
@@ -3,6 +3,7 @@ import "../../App.css";
 import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useNavigate, useParams } from "react-router-dom";
+import { HeartFilled, SmileOutlined } from "@ant-design/icons";
 
 const Search = () => {
   let nav = useNavigate();
@@ -14,39 +15,54 @@ const Search = () => {
     <div className="block" style={{ marginTop: "70px" }}>
       <div className="products" style={{ margin: "10px" }}>
         <Row gutter={[16, 16]}>
-          {products.map((product) => {
-            return (
-              <Col
-                sx={{ span: 24 }}
-                sm={{ span: 12 }}
-                md={{ span: 6 }}
-                key={product.key}
-                onClick={() => nav(`/${product.key}`)}
-              >
-                <Card
-                  hoverable
-                  bordered={false}
-                  cover={<img alt="example" src={product.img.mainImg} />}
+          {products.length === 0 ? (
+            <div style={{ minHeight: "400px", textAlign: "center" }}>
+              <h4 className="emptySearch">
+                We don't have this product in store! We will be update soon!
+                <HeartFilled />
+              </h4>
+              <h5 className="emptySearch">
+                Continue shopping?  
+                <a href="/#home"> Yes </a>
+                
+              </h5>
+            </div>
+          ) : (
+            products.map((product) => {
+              return (
+                <Col
+                  sx={{ span: 24 }}
+                  sm={{ span: 12 }}
+                  md={{ span: 6 }}
+                  key={product.key}
+                  onClick={() => nav(`/${product.key}`)}
                 >
-                  <h4 className="prosName">{product.name}</h4>
-                  <div className="currency">
-                    <h4>
-                      {product.price.toLocaleString("it-IT", {
-                        style: "currency",
-                        currency: "VND",
-                      })}
-                    </h4>
-                    <Button
-                      className="btnView"
-                      onClick={() => nav(`/${product.key}`)}
-                    >
-                      VIEW
-                    </Button>
-                  </div>
-                </Card>
-              </Col>
-            );
-          })}
+                  <Card
+                    hoverable
+                    bordered={false}
+                    cover={<img alt="example" src={product.img.mainImg} />}
+                  >
+                    <h4 className="prosName">{product.name}</h4>
+                    <div className="currency">
+                      <h4>
+                        {product.price.toLocaleString("it-IT", {
+                          style: "currency",
+                          currency: "VND",
+                        })}
+                      </h4>
+                      <Button
+                        className="btnView"
+                        onClick={() => nav(`/${product.key}`)}
+                      >
+                        VIEW
+                      </Button>
+                    </div>
+                  </Card>
+                </Col>
+              );
+            })
+          )}
+
           {/* <Pagination
               defaultCurrent={1}
               defaultPageSize={numEachPage} //default size of page
